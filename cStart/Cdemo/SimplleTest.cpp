2866f6104d4a9ff03ecdbbb614ab7707b15e88e9
LDJ <957058831@qq.com>
2022-07-10 22:09:08 +0800

    Demo

--- cStart/Cdemo/SimplleTest.cpp
+++ cStart/Cdemo/SimplleTest.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
-
+#include <variant>
+#include <variant>
+//1 . 测试decltyp(::函数)
 void decltype_func();
 void test(){
     std::cout<< "test\n";
-}
+};
+// 2. 测试variant C++17
+std::variant<int, float> test111;
+
 int main(){
 
     using type  = decltype(::decltype_func);// decltype 可以声明一个函数类型（类似于functional）
@@ -15,6 +20,18 @@ int main(){
     decltype(a) cc;// decltype 的第二种用法。
     cc= 10;
     std::cout<< "cc" << cc;
+    // 2.test
+
+    test111 = 2;
+    test111 = 1.0f;
+ 
+    try {
+        auto& res = std::get<int>(test111);
+    }
+    catch (const std::exception& e) {
+        std::cout<<"ss" << e.what();
+    }
+   
     return 0;
 }
 
