b3302e431f042b952358cf72801cc57288c04716
LDJ <957058831@qq.com>
2022-07-16 16:29:36 +0800

    1. void*

--- cStart/Cdemo/rtsacallBack.cpp
+++ cStart/Cdemo/rtsacallBack.cpp
@@ -3,9 +3,20 @@
 #include <iostream>
 #include <future>
 #include <string>
+#include <vector>
 
 typedef std::function<void(int ,const std::string& msg)> loghander;
 
+using error_callback_t = std::function<void(const std::string&)>;
+
+struct error_t {
+    using error_callback_t = void(__stdcall*)(void*, const char*);
+
+    void* impl_;
+    error_callback_t error_info_;
+};
+
+//1. Rtsa 中的log
 class engin;
 class workitem;
 
@@ -16,9 +27,7 @@ public:
     workitem() = default;
     void start();
     ~workitem() = default;
-
 private:
-
 };
 void workitem::start() {
     int i = 10;
@@ -29,6 +38,7 @@ void workitem::start() {
 // engine 
 class engin {
 				loghander log_;
+
     struct impl;
 public:
 				engin(const loghander &hand):log_(hand),impl_(std::make_unique<impl>(hand)){
@@ -48,6 +58,9 @@ private:
         }
         void start(){
             if (itemPtr!=nullptr) {
+
+           
+
                 log_(1,"some hanppen");
                 itemPtr->start();
             }
@@ -55,18 +68,33 @@ private:
     };
 
 };
+//2. 关于void*
 
+void print(void* pra){
+    auto ch = (char*) pra;
+    auto in = (int*) pra;
+    std::cout<<"in " <<*in <<std::endl;
+    std::cout<<"ch " <<*ch <<std::endl;
+
+}
 
 
 int main(){
+    // 1.test
     loghander handle = [](int num,const std::string &str){
         std::cout<<"num" << num <<std::endl;
         std::cout<<"str" <<str << std::endl;
     }; // 在外定义，构造注册
     std::unique_ptr<engin> engin1;
     engin1 = std::make_unique<engin>(handle);
-
     engin1->startItem();
+    // 2. test
+    //std:vector<int> ddd = {1,2,3,4};
+    std::vector<int> ss = {1545,2,3};
+    print(ss.data());
+
+    
+
 
     std::promise<void> p;
     auto fu = p.get_future();
